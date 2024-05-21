e87508b3d71d22bf565f9aa4ff47078ad5623b5f
13155a1 <ppy040@gmail.com>
2024-05-21 16:13:08 +0900

    feat: custom user 설정

---
+++ accounts/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .managers import UserManager
+
+class User(AbstractUser):
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
