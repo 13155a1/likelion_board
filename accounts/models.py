7641d2540addd9fb9925bfec6f93faf4b447a405
13155a1 <ppy040@gmail.com>
2024-05-21 22:00:56 +0900

    feat: 회원가입&로그인 구현

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,41 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager, AbstractBaseUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from .managers import UserManager
+class UserManager(BaseUserManager):
 
-class User(AbstractUser):
+    def create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError('Users must have an email address')
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+        user = self.model(
+            email=email,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        superuser = self.create_user(
+            email=email,
+            password=password,
+        )
+        superuser.is_staff = True
+        superuser.is_superuser = True
+        superuser.is_active = True
+        superuser.save(using=self._db)
+        return superuser
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    
+    email = models.EmailField(max_length=30, unique=True, null=False, blank=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = UserManager()
 
-    def __str__(self):
-        return self.username
+    USERNAME_FIELD = 'email'
