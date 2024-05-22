28a1d2f3bf464f0a8536c6170828ea2ab39e793e
13155a1 <ppy040@gmail.com>
2024-05-22 12:04:50 +0900

    feat: 모델 str 함수 추가

--- board/models.py
+++ board/models.py
@@ -6,11 +6,20 @@ class Post(models.Model):
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 class Comment(models.Model):
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.content
+
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.pk
